9313f69c5dcd4e5b855b4df65b72d186458da9aa
Carlos Henrique Escouto <carloshenriqueescouto@gmail.com>
2021-05-17 14:23:05 -0300

    feat(circuit-breaker): Adicionado duas implementações do pattern, uma usando lib e outro codada

--- microservices/ms-example/start/routes.ts
+++ microservices/ms-example/start/routes.ts
@@ -19,8 +19,12 @@
 */
 
 import Route from '@ioc:Adonis/Core/Route'
-import Notification from 'App/Entities/Notification'
 import UserModel from 'App/Models/UserModel'
+import Notification from 'App/Entities/Notification'
+// Circuit Breaker
+import CircuitBreaker from 'App/circuit-breaker/CircuitBreaker'
+import CircuitBreakerOpossum from 'opossum'
+import axios, { AxiosRequestConfig } from 'axios'
 
 Route.get('/', async () => {
   return { hello: 'world' }
@@ -34,4 +38,49 @@ Route.get('/init-kafka', async () => {
   console.log('finish')
 
   return user
+})
+
+//*
+// * Ciruito codado, na pasta APP
+const params: AxiosRequestConfig = {
+  method: 'get',
+  url: 'http://localhost:3333/test',
+}
+// *************************
+
+// *
+// * Ciruito da lib
+const circuitBreaker = new CircuitBreaker(params)
+
+const options = {
+  timeout: 3000,
+  errorThresholdPercentage: 50,
+  resetTimeout: 5000,
+}
+
+const circuitBreakerOpossum = new CircuitBreakerOpossum(axios, options)
+
+circuitBreakerOpossum.fallback(() =>
+  console.log('Serviço indisponivel no momento', circuitBreakerOpossum.status.stats)
+)
+// *************************
+
+Route.get('/circuit', async () => {
+  // circuitBreaker.exec().then(console.log).catch(console.error)
+
+  circuitBreakerOpossum
+    .fire('http://localhost:3333/test')
+    .then((result) => console.log(result.status))
+    .catch((result) => console.log(result.status))
+})
+
+Route.get('/test', async (data) => {
+  console.log('/test')
+  if (Math.random() > 0.2) {
+    console.log('####################### success ###########################')
+    data.response.status(200).send('Success!')
+  } else {
+    console.log('####################### fail ###########################')
+    data.response.status(400).send('Failed!')
+  }
 })
