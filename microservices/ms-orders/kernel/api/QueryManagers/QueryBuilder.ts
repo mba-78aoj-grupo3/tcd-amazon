be80dd4879aaf241b4d66b590a98ad3d272818c8
Carlos Henrique Escouto <carloshenriqueescouto@gmail.com>
2021-06-05 00:52:46 -0300

    ignore

--- microservices/ms-orders/kernel/api/QueryManagers/QueryBuilder.ts
+++ microservices/ms-orders/kernel/api/QueryManagers/QueryBuilder.ts
@@ -1,7 +1,6 @@
-
-import { isEmpty, forOwn, isObject, isNull, get } from 'lodash';
-import { Paginate } from '../Interfaces/index';
-import { Model } from '../ModelManagers/Model';
+import { isEmpty, forOwn, isObject, isNull, get } from 'lodash'
+import { Paginate } from '../Interfaces/index'
+import { Model } from '../ModelManagers/Model'
 
 /**
  *
@@ -10,32 +9,32 @@ export default class QueryBuilder {
   /**
    * @type {string}
    */
-  public query: string = '';
+  public query: string = ''
 
   /**
    * @type {Array<string>}
    */
-  public includes: Array<string> = [];
+  public includes: Array<string> = []
 
   /**
    * @type {string}
    */
-  public sort!: string;
+  public sort!: string
 
   /**
    * @type {Array<Record<string, string>>}
    */
-  public filters: Array<Record<string, string>> = [];
+  public filters: Array<Record<string, string>> = []
 
   /**
    * @type {Array<string>}
-   */  
-  public fields: Array<string> = [];
+   */
+  public fields: Array<string> = []
 
   /**
    * @type {Paginate}
    */
-  public pagination!: Paginate;
+  public pagination!: Paginate
 
   /**
    *
@@ -47,18 +46,17 @@ export default class QueryBuilder {
    * @returns string
    */
   public getQuery(self: Model): string {
-
-    this.query += this.resolveIncludes(self.queryBuilder.includes);
-    this.query += this.resolveFields(self.queryBuilder.fields);
-    this.query += this.resolveFilters(self.queryBuilder.filters);
-    this.query += this.resolvePagination(self.queryBuilder.pagination);
-    this.query += this.resolveSort(self.queryBuilder.sort);
+    this.query += this.resolveIncludes(self.queryBuilder.includes)
+    this.query += this.resolveFields(self.queryBuilder.fields)
+    this.query += this.resolveFilters(self.queryBuilder.filters)
+    this.query += this.resolvePagination(self.queryBuilder.pagination)
+    this.query += this.resolveSort(self.queryBuilder.sort)
 
     if (this.query.length) {
-      self.queryBuilder.query = `?${encodeURI(this.query)}`;
+      self.queryBuilder.query = `?${encodeURI(this.query)}`
     }
-    
-    return self.queryBuilder.query;
+
+    return self.queryBuilder.query
   }
 
   /**
@@ -68,19 +66,19 @@ export default class QueryBuilder {
     if (query) {
       return '&'
     }
-    return '';
+    return ''
   }
 
   /**
    * @param  {Model} self
    */
   public resetQuery(self: Model) {
-    self.queryBuilder.query = '';
-    self.queryBuilder.includes = [];
-    self.queryBuilder.sort = '';
-    self.queryBuilder.filters = [];
-    self.queryBuilder.fields = [];
-    self.queryBuilder.pagination = {number: NaN, size: NaN};
+    self.queryBuilder.query = ''
+    self.queryBuilder.includes = []
+    self.queryBuilder.sort = ''
+    self.queryBuilder.filters = []
+    self.queryBuilder.fields = []
+    self.queryBuilder.pagination = { number: NaN, size: NaN }
   }
 
   /**
@@ -88,14 +86,14 @@ export default class QueryBuilder {
    * @returns string
    */
   public resolveFields(fields: any): string {
-    let resolveFields = '';
+    let resolveFields = ''
 
     forOwn(fields, (fields, resource) => {
-      resolveFields += `fields[${resource}]=${fields.toString()}`;
-    });
+      resolveFields += `fields[${resource}]=${fields.toString()}`
+    })
 
     if (!isEmpty(resolveFields)) {
-      return `${this.setAmpersand(this.query)}${resolveFields}`;
+      return `${this.setAmpersand(this.query)}${resolveFields}`
     }
 
     return ''
@@ -106,15 +104,15 @@ export default class QueryBuilder {
    * @returns string
    */
   public resolveFilters(filters: Array<Record<string, string>>): string {
-    let resolveFilters = '';
+    let resolveFilters = ''
 
     filters.map((filter: Record<string, string>, index: number) => {
-      const property = Object.getOwnPropertyNames(filter)[0];
-      resolveFilters += `${(index >= 1 ? '&' : '')}filter[${property}]=${filter[property].toString()}`;
+      const property = Object.getOwnPropertyNames(filter)[0]
+      resolveFilters += `${index >= 1 ? '&' : ''}filter[${property}]=${filter[property].toString()}`
     })
 
     if (!isEmpty(resolveFilters)) {
-      return `${this.setAmpersand(this.query)}${resolveFilters}`;
+      return `${this.setAmpersand(this.query)}${resolveFilters}`
     }
 
     return ''
@@ -126,7 +124,7 @@ export default class QueryBuilder {
    */
   public resolveIncludes(includes: Array<string>): string {
     if (!isEmpty(includes)) {
-      return `${this.setAmpersand(this.query)}include=${includes.toString()}`;
+      return `${this.setAmpersand(this.query)}include=${includes.toString()}`
     }
 
     return ''
@@ -138,9 +136,11 @@ export default class QueryBuilder {
    */
   public resolvePagination(pagination: Paginate): string {
     if (get(pagination, 'number', false) && get(pagination, 'size', false)) {
-      return `${this.setAmpersand(this.query)}page[size]=${pagination.size}&page[number]=${pagination.number}`;
+      return `${this.setAmpersand(this.query)}page[size]=${pagination.size}&page[number]=${
+        pagination.number
+      }`
     }
-    
+
     return ''
   }
 
@@ -150,7 +150,7 @@ export default class QueryBuilder {
    */
   public resolveSort(sort: string): string {
     if (!isEmpty(sort)) {
-      return `${this.setAmpersand(this.query)}sort=${sort}`;
+      return `${this.setAmpersand(this.query)}sort=${sort}`
     }
 
     return ''
